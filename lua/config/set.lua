1de12d8f0990f7e9fa46d25e0b6ca4c4f74eed8f
jorenchik <jorens.stekels@gmail.com>
2023-08-09 07:04:18 +0300

    feat: added vim set

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,13 +1,26 @@
 -- Vim startup settings
 vim.opt.ignorecase = true
-vim.opt.autoindent = true
 vim.opt.wrap = false
-vim.opt.textwidth = 95
-vim.opt.colorcolumn = "-2"
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.colorcolumn = "80"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.tm = 100
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
 vim.cmd.colorscheme("catppuccin")
