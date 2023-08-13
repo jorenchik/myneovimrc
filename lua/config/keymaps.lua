c7155455305cfd6899c7f59d3f61c7283baf35b5
jorenchik <jorens.stekels@gmail.com>
2023-08-13 09:01:30 +0300

    feat: selection formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,11 +17,11 @@ wk.register({
 
 -- code
 vim.keymap.set("n", "<Leader>cm", "<cmd>Mason<cr>", { desc = "mason" })
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"v",
 	"<leader>cf",
-	'<cmd>execute "Neoformat! " . &filetype<CR>',
-	{ desc="Format the selection", noremap = true, silent = true }
+	"<cmd>'<,'>Neoformat<cr>",
+	{ desc = "Format the selection", noremap = true, silent = true }
 )
 vim.keymap.set("n", "<Leader>cf", "<cmd>Neoformat<cr>", { desc = "Format the buffer" })
 vim.keymap.set("n", "<Leader>cc", "<cmd>:Cheatsheet<cr>", { desc = "VIM cheatsheet" })
