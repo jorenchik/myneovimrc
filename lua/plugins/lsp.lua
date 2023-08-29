52000c5479f84ede4fa043e9e6b3629ef33a6a60
jorenchik <jorens.stekels@gmail.com>
2023-08-29 07:16:17 +0300

    feat[lsp]: friendly snippets for luasnip

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,6 +8,8 @@ return {
 				{ "neovim/nvim-lspconfig" },
 				{ "williamboman/mason.nvim" },
 				{ "williamboman/mason-lspconfig.nvim" },
+				{ "rafamadriz/friendly-snippets" },
+				{ "saadparwaiz1/cmp_luasnip" },
 
 				-- Autocompletion
 				{ "hrsh7th/nvim-cmp" },
@@ -15,13 +17,20 @@ return {
 				{ "L3MON4D3/LuaSnip" },
 			},
 			config = function()
-				local lsp = require("lsp-zero").preset({})
+				local lsp = require("lsp-zero").preset({
+					manage_nvim_cmp = { set_sources = "recommended" },
+				})
 
-				lsp.on_attach(function(client, bufnr)
+				lsp.on_attach(function(_, bufnr)
 					lsp.default_keymaps({ buffer = bufnr })
 				end)
 
 				lsp.nvim_workspace()
+
+				local cmp_action = require("lsp-zero").cmp_action()
+
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_snipmate").lazy_load()
 
 				local cmp = require("cmp")
 				local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -30,46 +39,64 @@ return {
 					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-f>"] = cmp_action.luasnip_jump_forward(),
+					["<C-b>"] = cmp_action.luasnip_jump_backward(),
 				})
 
-				lsp.setup_nvim_cmp({
+				require("luasnip").filetype_extend("html", { "djangohtml" })
+
+				cmp.setup({
+					sources = {
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+					},
 					mapping = cmp_mappings,
 				})
 
-				lsp.on_attach(function(client, bufnr)
+				lsp.on_attach(function(_, bufnr)
 					local opts = { buffer = bufnr, remap = false }
 
 					vim.keymap.set("n", "gd", function()
 						vim.lsp.buf.definition()
 					end, { unpack(opts), desc = "Go to definition" })
+
 					vim.keymap.set("n", "K", function()
 						vim.lsp.buf.hover()
 					end, { unpack(opts), desc = "Show information" })
+
 					vim.keymap.set("n", "<leader>vws", function()
 						vim.lsp.buf.workspace_symbol()
 					end, { unpack(opts), desc = "Workspace symbol" })
+
 					vim.keymap.set("n", "<leader>vd", function()
 						vim.diagnostic.open_float()
 					end, { unpack(opts), desc = "Open floating diagnostic" })
+
 					vim.keymap.set("n", "[d", function()
 						vim.diagnostic.goto_next()
 					end, { unpack(opts), desc = "Go to next" })
+
 					vim.keymap.set("n", "]d", function()
 						vim.diagnostic.goto_prev()
 					end, { unpack(opts), desc = "Go to previous" })
+
 					vim.keymap.set("n", "<leader>vca", function()
 						vim.lsp.buf.code_action()
 					end, { unpack(opts), desc = "action" })
+
 					vim.keymap.set("n", "<leader>vrr", function()
 						vim.lsp.buf.references()
 					end, { unpack(opts), desc = "Show references" })
+
 					vim.keymap.set("n", "<leader>vrn", function()
 						vim.lsp.buf.rename()
 					end, { unpack(opts), desc = "Rename" })
+
 					vim.keymap.set("i", "<C-h>", function()
 						vim.lsp.buf.signature_help()
 					end, { unpack(opts), desc = "Signature help" })
 				end)
+
 				lsp.setup()
 			end,
 		},
